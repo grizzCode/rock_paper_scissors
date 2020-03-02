1bba0b6d4e460869e8985f8da5771f517c8d1222
Charles Buxton <chazbux@gmail.com>
2020-03-02 16:47:08 -0700

    getting better...

--- scripts.js
+++ scripts.js
@@ -1,31 +1,53 @@
+
 let arr = ["Rock", "Paper", "Scissors"];
-let x
 
-  document.getElementById("rock").onclick = function() {rockFn()};
-  document.getElementById("paper").onclick = function() {paperFn()};
-  document.getElementById("scissors").onclick = function() {scissorsFn()};
-
-
-function rockFn() {
-  console.log(arr[0])
-  x = arr[0]
-  return 0
-}
-function paperFn() {
-  console.log(arr[1])
-  x = arr[1]
-  return 1
-}
-function scissorsFn() {
-  console.log(arr[2])
-  x = arr[2]
-  return 2
-}
-
-console.log(x)
-
+function getComputer() {
 let computer = arr[Math.floor(Math.random()*arr.length)];
 document.getElementById('computer').innerHTML = `Computer: ${computer}`
+return computer
+}
+
+function playGame(player, computer) {
+  if (player === computer) {
+    console.log("Tie")
+    return "Tie"
+  }
+  if (player === "Paper") {
+    if (computer === "Rock") {  
+      console.log("Player wins!"); 
+      return 
+    } else if (computer === "Scissors") {   
+      console.log("Computer wins...");  
+      return 
+    }
+  }
+  if (player === "Scissors") {
+      if (computer === "Rock") {
+        console.log("Computer wins...");  
+        return "Computer wins.";
+      } else {
+          if (computer === "Paper") {
+            console.log("Player wins!"); 
+            return "Player wins!";
+          }
+      }
+    }
+  else if (player === "Rock") {
+    if (computer === "Paper") {
+        console.log("Computer wins...");
+        return;
+    } else {
+        if (computer === "Scissors") {
+          console.log("Player wins!"); 
+          return;
+        }
+    }
+  }
+}
 
 
+document.getElementById("rock").onclick = function() {playGame("Rock", getComputer())};
+document.getElementById("paper").onclick = function() {playGame("Paper", getComputer())};
+document.getElementById("scissors").onclick = function() {playGame("Scissors", getComputer())};
 
+document.getElementById('computer').innerHTML = `Computer: ${computer}`
